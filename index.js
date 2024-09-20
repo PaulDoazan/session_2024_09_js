338f52c074853ae646490e600f972e41d308c6fd
pdoazan <paul.doazan@gmail.com>
2024-09-20 12:36:53 +0200

    Add myIdentity

--- index.js
+++ index.js
@@ -38,17 +38,60 @@ result += 3
 const allowed = false
 const notAllowed = false
 
-if (allowed) {
-    console.log('On est passé dans la condition')
-} else {
-    console.log('On est passé dans le else de la condition')
-}
+// if (allowed) {
+//     console.log('On est passé dans la condition')
+// } else {
+//     console.log('On est passé dans le else de la condition')
+// }
 
 const legalAgeToDrive = 18
-const myAge = 36
+const myAge = "18"
 
-if () {
-    console.log('Tu peux conduire')
-} else {
-    console.log('Tu ne peux pas conduire')
+firstname = ''
+
+// les opérateurs de comparaison : < > <= >= == === != !==
+// if (myAge >= legalAgeToDrive) {
+//     console.log('Tu peux conduire')
+// } else {
+//     console.log('Tu ne peux pas conduire')
+// }
+
+// if (myAge != legalAgeToDrive) {
+//     console.log(`Tu n'as pas 18 ans`)
+// } else {
+//     console.log(`Tu viens d'être majeur`)
+// }
+
+
+// 4 VARIABLE COMPLEXE : LES TABLEAUX, ARRAYS
+const myArray = ["Bordeaux", "Mérignac", "Pessac"]
+myArray[2] = "Bruges"
+myArray.push('Bègles')
+myArray.push('Cenon')
+myArray.push('Lormont')
+myArray.push('Eysines')
+
+myArray[5] = 31
+myArray[3] = "Gradignan"
+
+// Modifier par "Gradignan" le 4è élément du tableau
+
+
+// 5 VARIABLE COMPLEXE : LES OBJETS, OBJECTS
+const myIdentity = {
+    firstname: ["Paul", "Pierre"],
+    lastname: 'Doazan',
+    age: 36,
+    town: {
+        name: "Bordeaux",
+        population: 300000,
+        districts: ["Pessac", "Merignac", "Talence"]
+    }
 }
+
+myIdentity.town.districts.push('Gradignan')
+
+// console.log(myIdentity.town.districts[myIdentity.town.districts.length - 1])
+
+// A l'aide de l'objet myIdentity, construire la phrase "Bonjour, je m'appelle Paul Doazan, j'ai 36 ans, et j'habite à Talence".
+console.log(myIdentity.firstname)
