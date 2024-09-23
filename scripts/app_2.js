ea95195f2d0ff24a51e1b425de5fde4bb8744d5a
pdoazan <paul.doazan@gmail.com>
2024-09-23 15:28:39 +0200

    Update html css

--- scripts/app_2.js
+++ scripts/app_2.js
@@ -1,19 +1,18 @@
 // 1ere étape, on récupère les balises html dans des variables
 const btnDiv = document.querySelector('.button')
+const titleDiv = document.querySelector('.title')
 const body = document.querySelector('body')
 
+// 2ème étape, on déclare un click sur le bouton
 btnDiv.addEventListener('click', randomColor)
 
+// 3ème étape, on implémente la fonction randomColor qui est exécutée à chaque click sur le bouton
 function randomColor() {
     const red = Math.random() * 255
     const green = Math.random() * 255
     const blue = Math.random() * 255
 
-    // color
-
+    titleDiv.style.color = `rgb(${255 - red},${255 - green},${255 - blue})`
     body.style.backgroundColor = `rgb(${red},${green},${blue})`
 }
 
-// 2ème étape, on déclare un click sur le bouton
-
-// 3ème étape, on implémente la fonction randomColor qui est exécutée à chaque click sur le bouton
