2ce3cb7d10a9889a06fb2e47decb98c49b0eea8b
pdoazan <paul.doazan@gmail.com>
2024-09-24 09:58:57 +0200

    Add numbers array

--- scripts/index_3.js
+++ scripts/index_3.js
@@ -1,1 +1,22 @@
-console.log('index 3')
+let sentence = sayHello("David")
+sentence += ' !!!'
+
+let sentence2 = sayHello("Paul")
+sentence2 += ' !!!'
+
+// Après un return, le code n'est pas exécuté
+function sayHello(name) {
+    if (name === "Paul") {
+        return ''
+    }
+
+    return `Hello and welcome ${name}`
+}
+
+
+// écrire la fonction qui prend en paramètre un tableau et retourne la moyenne des éléments du tableau
+const nbs = [21, 23, 24, 26, 83, 81, 1, 4]
+
+function average() {
+
+}
