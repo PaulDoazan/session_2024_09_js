4f04a3f479ff99e4858ba0e333131d721747f79d
pdoazan <paul.doazan@gmail.com>
2024-09-24 12:32:23 +0200

    Add window.innerWidth

--- scripts/app_3.js
+++ scripts/app_3.js
@@ -6,15 +6,27 @@ const bgContainer = document.querySelector(".background-container")
 btn.addEventListener('click', onBtnClick)
 btn.addEventListener('click', addNewElement)
 
+let addedElements = []
+
 function addNewElement() {
+    // window est un objet natif Javascript qui donne accès à la fenêtre du navigateur
+    // la propriété innerWidth donne la largeur de la fenêtre en pixels
+    console.log(window.innerWidth)
     // méthode createElement pour créer une nouvelle balise html
     const newDiv = document.createElement("div");
+    addedElements.push(newDiv)
 
     // on met à jour la propriété textContent de la nouvelle balise
     newDiv.textContent = "Ma nouvelle div"
 
     // on ajoute une classe css à la nouvelle balise, les différentes propriétés css de cette classe seront donc appliquées
     newDiv.classList.add('stripe')
+    const color = randomColor()
+    newDiv.style.backgroundColor = `rgb(${color[0]},${color[1]},${color[2]})`
+    for (let i = 0; i < addedElements.length; i++) {
+        const element = addedElements[i];
+        element.style.width = `${window.innerWidth / addedElements.length}px`
+    }
 
     // on insère la nouvelle balise dans la balise bgContainer
     bgContainer.appendChild(newDiv)
@@ -33,4 +45,12 @@ function onBtnClick() {
     } else {
         myTitle.textContent = "Au revoir !"
     }
+}
+
+function randomColor() {
+    const red = Math.random() * 255
+    const green = Math.random() * 255
+    const blue = Math.random() * 255
+
+    return [red, green, blue]
 }
