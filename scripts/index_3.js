cf4699fbd3616f95f922deff9cc62077141092ae
pdoazan <paul.doazan@gmail.com>
2024-09-24 10:15:06 +0200

    Add habitants

--- scripts/index_3.js
+++ scripts/index_3.js
@@ -15,8 +15,54 @@ function sayHello(name) {
 
 
 // écrire la fonction qui prend en paramètre un tableau et retourne la moyenne des éléments du tableau
-const nbs = [21, 23, 24, 26, 83, 81, 1, 4]
+const nbs = [21, 23, 24, 26, 83, 81, 1, 4, 7, 13]
+console.log(average(nbs))
 
-function average() {
+function average(arr) {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
 
+    return Math.round(sum / arr.length)
 }
+
+const habitants = [
+    {
+        name: "Jacques",
+        age: 25,
+        town: "Paris"
+    },
+    {
+        name: "Simon",
+        age: 70,
+        town: "Paris"
+    },
+    {
+        name: "Jeanne",
+        age: 12,
+        town: "Paris"
+    },
+    {
+        name: "Marianne",
+        age: 38,
+        town: "Paris"
+    },
+    {
+        name: "Marianne",
+        age: 43,
+        town: "Bordeaux"
+    },
+    {
+        name: "Paul",
+        age: 65,
+        town: "Bordeaux"
+    },
+    {
+        name: "Pierre",
+        age: 91,
+        town: "Bordeaux"
+    },
+]
+
+// Ecrire une fonction qui prends [habitants] en 1er paramètre et une ville en second paramètre, et qui retourne un tableau des noms des habitants de cette ville
