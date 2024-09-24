b18d0f0491368773fdf9db9249879f431e47fb54
pdoazan <paul.doazan@gmail.com>
2024-09-24 12:50:32 +0200

    Add chessboard colors

--- scripts/app_3.js
+++ scripts/app_3.js
@@ -7,29 +7,41 @@ btn.addEventListener('click', onBtnClick)
 btn.addEventListener('click', addNewElement)
 
 let addedElements = []
+let lastRowCreated
+let count = 0
 
 function addNewElement() {
+    if (count === 0) {
+        const newRow = document.createElement("div");
+        lastRowCreated = newRow
+        newRow.classList.add('stripe-row')
+        bgContainer.appendChild(newRow)
+    }
+
     // window est un objet natif Javascript qui donne accès à la fenêtre du navigateur
     // la propriété innerWidth donne la largeur de la fenêtre en pixels
-    console.log(window.innerWidth)
+    // console.log(window.innerWidth)
     // méthode createElement pour créer une nouvelle balise html
     const newDiv = document.createElement("div");
     addedElements.push(newDiv)
 
     // on met à jour la propriété textContent de la nouvelle balise
-    newDiv.textContent = "Ma nouvelle div"
+    // newDiv.textContent = "Ma nouvelle div"
 
     // on ajoute une classe css à la nouvelle balise, les différentes propriétés css de cette classe seront donc appliquées
     newDiv.classList.add('stripe')
     const color = randomColor()
     newDiv.style.backgroundColor = `rgb(${color[0]},${color[1]},${color[2]})`
-    for (let i = 0; i < addedElements.length; i++) {
-        const element = addedElements[i];
-        element.style.width = `${window.innerWidth / addedElements.length}px`
-    }
+    // for (let i = 0; i < addedElements.length; i++) {
+    //     const element = addedElements[i];
+    //     element.style.width = `${window.innerWidth / addedElements.length}px`
+    // }
 
     // on insère la nouvelle balise dans la balise bgContainer
-    bgContainer.appendChild(newDiv)
+    lastRowCreated.appendChild(newDiv)
+
+    count++
+    if (count === 10) count = 0
 }
 
 
